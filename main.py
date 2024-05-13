585959da1c675c3ed231450a94143a37ad3678d9
jucajata <jcjt95@gmail.com>
2024-05-13 15:01:09 -0500

    feat: Configurar páginas de error

--- main.py
+++ main.py
@@ -7,12 +7,25 @@ app = Flask(__name__)
 
 todos = ['Comprar café', 'Enviar solicitud de compra', 'Entregar vídeo al productor']
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+@app.errorhandler(500)
+def internal_server_error(error):
+  return render_template('500.html', error=error)
+    
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr
     response = make_response(redirect('/hello'))
     response.set_cookie('user_ip', user_ip)
     return response
+
+@app.route('/server_error')
+def server_error():
+    raise(Exception('500 error'))
     
 
 @app.route('/hello')
